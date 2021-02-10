5036487c9cf83c812f0b7c73d12ca92df5e46002
flecorre <lecorre.f@gmail.com>
2021-02-10 13:37:34 +0000

    test function for environment builtin and better env function

--- test/maintest.c
+++ test/maintest.c
@@ -4,49 +4,45 @@ int main(int ac, char *av[], char **ep)
 {
 	if (ac > 1)
 		printf("%s", *av);
-	/* env function test
-    char    **test;
     t_list    *list;
     list = create_env_list(ep);
-
-    add_env_variable(&list, ft_strdup("TEST=TOTO"));
-    add_env_variable(&list, ft_strdup("TEST=RITOTO"));
-    add_env_variable(&list, ft_strdup("TATA=tutu"));
-    add_env_variable(&list, ft_strdup("TITI=tutu"));
-    printf("this is the Value for TITI: %s\n", find_env_value(&list, "TITI"));
-    delete_env_variable(&list, "TITI");
-    test = env_list_to_tab(list);
-
-    ft_lstdel(&list, free_env);
-
-
-    while (*test != NULL)
-    {
-        printf("%s\n", *test);
-        test++;
-    }
-*/
 	t_command *cmd;
-	(void)ep;
 	int			i = 0;
 
 	cmd = malloc(sizeof(t_command));
 	cmd->command = NULL;
-	cmd->args = malloc(sizeof(char *) * 6);
-	bzero(cmd->args, (sizeof(char *) * 6));
+	cmd->command = malloc(sizeof(char *) * 6);
+	bzero(cmd->command, (sizeof(char *) * 6));
 	while (i < 3)
 	{
 		cmd->fd[i] = i;
 		i++;
 	}
-	cmd->args[0] = "-nnnnnn";
-	cmd->args[1] = "-n";
-	cmd->args[2] = "nice";
-	cmd->args[3] = "yop";
-	cmd->args[4] = "lulz";
+
+	cmd->command[0] = "tata";
+
+	env_builtin(&list, cmd);
+	cmd->command[1] = "TEST=TOTO";
+	export_builtin(&list, cmd);
+	cmd->command[1] = "TEST=RITOTO";
+	export_builtin(&list, cmd);
+	cmd->command[1] = "TATA=tutu";
+	export_builtin(&list, cmd);
+	cmd->command[1] = "TITI=tutu";
+	export_builtin(&list, cmd);
+
+
+	env_builtin(&list, cmd);
+
+    ft_lstdel(&list, free_env);
+	cmd->command[0] = "-nnnnnn";
+	cmd->command[1] = "-n";
+	cmd->command[2] = "nice";
+	cmd->command[3] = "yop";
+	cmd->command[4] = "lulz";
 	echo_builtin(NULL, cmd);
-	cmd->args -= 5;
-	cmd->args[0] = "n";
+	cmd->command -= 5;
+	cmd->command[0] = "n";
 	echo_builtin(NULL, cmd);
     return(0);
 }
