845bd8296b82e56620a618d197b464497e10c045
jfreitas <jfreitas@student.s19.be>
2021-02-12 23:38:04 +0100

    Executable builtins DONE

--- srcs/executable_builtin.c
+++ srcs/executable_builtin.c
@@ -6,18 +6,17 @@
 /*   By: jfreitas <jfreitas@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 19:16:50 by jfreitas          #+#    #+#             */
-/*   Updated: 2021/02/12 14:52:42 by jfreitas         ###   ########.fr       */
+/*   Updated: 2021/02/12 23:40:27 by jfreitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-//#include "../includes/minishell.h"
 
-//////////////////////////////// STILL WORKING ON IT ////////////////////////////////
-void	executable_parent(pid_t fork_pid)
+void	executable_parent(t_command *cmd, pid_t fork_pid)
 {
 	int		wstatus;
 
+	free(cmd->command[0]);//later to be replaced for a function that frees the t_cmd (or at least the **command?)
 	wstatus = 0;
 	waitpid(fork_pid, &wstatus, 0);
 	if (WIFEXITED(wstatus))//macro that returns true if the child terminated normally
@@ -39,7 +38,6 @@ char	*find_absolute_path(char *cmd, char *env_path)
 	i = -1;
 	fd = 0;
 	each_path_dir = ft_strsplit(env_path, ':');
-	add_path_to_cmd = NULL;
 	while (each_path_dir[++i])
 	{
 		add_slash_to_path = ft_strjoin(each_path_dir[i], "/");
@@ -58,11 +56,41 @@ char	*find_absolute_path(char *cmd, char *env_path)
 	return (add_path_to_cmd);
 }
 
+char	*relative_path(char *cmd, char *env_path)
+{
+	char	**each_path_dir;
+	char	*cmd_without_tilde;
+	char	*add_path_till_root_to_cmd;
+	int		i;
+	int		fd;
+
+	i = -1;
+	fd = 0;
+	each_path_dir = ft_strsplit(env_path, ':');
+	while (each_path_dir[++i])
+	{
+		cmd_without_tilde = ft_strdup(&cmd[1]);
+		add_path_till_root_to_cmd = ft_strjoin("../..", cmd_without_tilde);
+		if ((fd = open(add_path_till_root_to_cmd, O_RDONLY)) == -1)
+		{
+			ft_strdel(&cmd_without_tilde);
+			ft_strdel(&add_path_till_root_to_cmd);
+		}
+		else
+			break ;
+	}
+	ft_strdel(each_path_dir);
+	if (fd == 1)
+		close(fd);
+	return (add_path_till_root_to_cmd);
+}
+
 char	*path_to_executable(t_list **list, t_command *cmd)
 {
 	char	*absolute_path;
 	char	*env_path;
 
+	signal(SIGQUIT, ctrl_back_slash_handler_quit);
 	env_path = find_env_value(list, "PATH");
 	if (!cmd->command)
 		return (NULL);
@@ -71,24 +99,25 @@ char	*path_to_executable(t_list **list, t_command *cmd)
 		ft_strncmp(cmd->command[0], "~/", 2) != 0)
 	{
 		if ((absolute_path = find_absolute_path(cmd->command[0], env_path)) == NULL)
-		{
-		//	error_msg(cmd, -1, "");//0 = does not write "bash:"
 			return ("exit");
-		}
 	}
 	else
 	{
-		absolute_path = ft_strdup(cmd->command[0]);
-		if (open(cmd->command[0], O_RDONLY) == -1)
-		{
-		//	error_msg_bash(cmd, -1, "");//1 = writes "bash:"
+		if (ft_strncmp(cmd->command[0], "~/", 2) == 0)
+			absolute_path = relative_path(cmd->command[0], env_path);
+		else
+			absolute_path = ft_strdup(cmd->command[0]);
+		if (open(absolute_path, O_RDONLY) == -1)
 			return ("exit_bash");
-		}
 	}
-//	exit(127);//or exit(errno);????? NOT HERE
 	return (absolute_path);
 }
 
+/*
+ * Fork:
+ * On success, the PID of the child process is returned in the parent, and 0 is returned in the child.
+ * On failure, -1 is returned in  the  parent, no child process is created, and errno is set appropriately.
+ */
 int		executable_builtin(t_list **head, t_command *cmd)
 {
 	char	**env;
@@ -97,23 +126,23 @@ int		executable_builtin(t_list **head, t_command *cmd)
 
 	env = env_list_to_tab(*head);
 	path_to_cmd = path_to_executable(head ,cmd);
-	signal(SIGQUIT, ctrl_back_slash_handler_quit);
-	if ((fork_pid = fork()) == -1)//On failure, -1 is returned in  the  parent, no child process is created, and errno is set appropriately.
-		exit(2);//or -1? or errno???
-	else if (fork_pid == 0)//  On success, the PID of the child process is returned in the parent, and 0 is returned in the child
+	if ((fork_pid = fork()) == -1)
+		exit(errno);// or -1???
+	else if (fork_pid == 0)
 	{
 		if (execve(path_to_cmd, cmd->command, env) == -1)
 		{
 			if (ft_strcmp(path_to_cmd, "exit_bash") == 0)
-				error_msg_bash(cmd, 127, "");//bash: /bin/lo: No such file or directory
+			{
+				errno = 2;
+				error_msg_bash(cmd, -1, "");//bash: <cmd>: No such file or directory (errno 2 for this message)
+			}
 			if (ft_strcmp(path_to_cmd, "exit") == 0)
-				error_msg(cmd, 127, "");//lo: command not found
+				error_msg(cmd, 127, "command not found\n");//<cmd>: command not found (there's no errno number for this message)
+			return (127);
 		}
 	}
 	else
-	{
-		free(cmd->command[0]);//later to be replaced for a function that frees the t_cmd (or at least the **command?)
-		executable_parent(fork_pid);
-	}
+		executable_parent(cmd, fork_pid);
 	return (0);
 }
